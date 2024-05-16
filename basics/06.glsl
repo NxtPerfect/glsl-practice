edc8d5e6d471be5dd400c9e1ec4c2cc8ce6d1925
NxtPerfect <alakaxan@protonmail.com>
2024-05-16 09:58:16 +0200

    White triangle with blue circle

---
+++ basics/06.glsl
@@ -0,0 +1,42 @@
+/* White triangle from left side of the screen pointing to the right side
+ * with blue circle inside
+*/
+float col(vec2 a, vec2 b, vec2 uv) {
+    float cos1 = dot(a, uv);
+    float cos2 = dot(b, uv);
+    float cos3 = dot(a, b);
+    
+    return float ((cos1 >= cos3)) * float ((cos2 >= cos3));
+}
+
+void mainImage( out vec4 fragColor, in vec2 fragCoord) {
+
+    vec2 uv = fragCoord / iResolution.xy;
+    
+    vec2 a = vec2(0.,0.);
+    vec2 b = vec2(1.,.5);
+    vec2 c = vec2(0., 1.);
+    
+    vec2 ab = a - b;
+    vec2 bc = b - c;
+    vec2 ac = a - c;
+    
+    float q1 = col(normalize(ab), normalize(ac), -normalize(uv - a));
+    float q2 = col(normalize(ab), normalize(bc), -normalize(uv - b));
+    
+    vec3 triangleColor = vec3(1., 1., 1.);
+    float triangle = q1 - q2;
+    
+    float circleRadius = .25;
+    vec2 circleCenter = vec2(.5, .5);
+    vec3 circleColor = vec3(0., 0., 1.);
+    float aspectRatio = iResolution.x / iResolution.y;
+    float maxDimension = max(iResolution.x, iResolution.y);
+    float distanceToCenter = length(uv * vec2(aspectRatio, 1.0) - circleCenter * vec2(aspectRatio, 1.0))*maxDimension;
+   
+    float circle = smoothstep(circleRadius * maxDimension, (circleRadius + 0.01) * maxDimension, distanceToCenter);
+    
+    vec3 col = mix(circleColor, triangleColor * triangle, circle);
+    
+    fragColor = vec4(col, 1.0);
+}
